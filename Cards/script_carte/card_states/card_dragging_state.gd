1a6e268eb96e02b295d0ad0a3db854578c156538
Emmanuele_Cogoni <cogoni279@gmail.com>
2025-07-10 15:33:24 +0200

    Prima versione carte drag&drop
    Prima versione carte , drag and drop

---
+++ Cards/script_carte/card_states/card_dragging_state.gd
@@ -0,0 +1,29 @@
+extends CardState
+#dragging state
+const DRAG_MINUMUN_THRESHOLD:= 0.05
+var minimum_drag_time_elapsed:=false
+
+func enter() -> void:
+	var ui_layer=get_tree().get_first_node_in_group("ui_layer")
+	if ui_layer:
+		card_ui.reparent(ui_layer)
+		
+	card_ui.color.color=Color.NAVY_BLUE
+	card_ui.state.text="DRAGGING"
+	
+	minimum_drag_time_elapsed=false
+	var threshold_timer:=get_tree().create_timer(DRAG_MINUMUN_THRESHOLD,false)
+	threshold_timer.timeout.connect(func(): minimum_drag_time_elapsed=true)
+
+func on_input(event: InputEvent)->void:
+	var mouse_motion:= event is InputEventMouseMotion
+	var cancel=event.is_action_pressed("right_click")
+	var confirm=event.is_action_pressed("left_click") or event.is_action_released("right_click")
+	
+	if mouse_motion:
+		card_ui.global_position=card_ui.get_global_mouse_position()-card_ui.pivot_offset
+	if cancel:
+		transition_requested.emit(self,CardState.State.BASE)
+	elif confirm and minimum_drag_time_elapsed:
+		get_viewport().set_input_as_handled()
+		transition_requested.emit(self,CardState.State.RELEASED)
---
+++ Cards/script_carte/card_states/card_released_state.gd
@@ -0,0 +1,17 @@
+extends CardState
+#released state
+var played:bool
+
+func enter() -> void:
+	card_ui.color.color=Color.DARK_VIOLET
+	card_ui.state.text="RELEASED"
+	
+	played=false
+	
+	if not card_ui.targets.is_empty():
+		played=true
+
+func on_input(_event: InputEvent)->void:
+	if played:
+		return
+	transition_requested.emit(self,CardState.State.BASE)
